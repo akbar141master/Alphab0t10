2f306aef45dc9280889c5b449558fbc05ca50a62
akbar141master <74704480+akbar141master@users.noreply.github.com>
2021-11-14 18:27:35 +0530

    Add files via upload

--- zeeone/zeeoneofc.js
+++ zeeone/zeeoneofc.js
@@ -110,7 +110,7 @@ ${petik}└❒ ${prefix}anime${petik} _random_
 `
 }
 const allmenu = (prefix, wita, wit, ucapannya2, timuu, status, wa_version, mcc, mnc, os_version, device_manufacturer, device_model, alfa , alfa1, thisDay, ini_tanggal, totalchat, hit_today, ini_gcchat, latensiii, tekss)  => {
-	return`❏───「 *ALPHA BOT* 」───❏
+	return`❏───「 *𝙈𝙊𝙉𝙎𝙏𝙀𝙍  𝘽𝙊𝙏* 」───❏
      
 ┌❏ *INDONESIA TIME*
 ${petik}│◦➛ WIB : ${timuu}
@@ -672,7 +672,7 @@ ${petik}│◦ ${prefix}rikagusriani
 │
 └─❑「 MENU BOT 」
 
-❏───「 *ALPHA BOT* 」───❏
+❏───「 *𝙈𝙊𝙉𝙎𝙏𝙀𝙍  𝘽𝙊𝙏* 」───❏
 `
 }
 
